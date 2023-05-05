9d908d3b175aad6952e910f0f8fd6a0736d7401d
Nima Abed <abodnima84@gmail.com>
2023-05-05 10:18:17 +0330

    Create If For Tie

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const computerChoise = choises[Math.round(Math.random() *3)]
 
 if (!(userChoise === "")){
     if(userChoise === "rock" || userChoise === "paper" || userChoise === "scissors"){
+        if(userChoise === computerChoise){
+            console.log("It's Tie")
+        }else{
 
+        }
     }else{
         console.log("You Wrote Wrong")
     }
