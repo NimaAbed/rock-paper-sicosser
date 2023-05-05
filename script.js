df1814ab55bba160ee16d0b6a4617b58c007a564
Nima Abed <abodnima84@gmail.com>
2023-05-05 10:15:36 +0330

    Create If For You Wrote Wrong

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const choises = ["rock","paper","scissors"]
 const computerChoise = choises[Math.round(Math.random() *3)]
 
 if (!(userChoise === "")){
-    
+    if(userChoise === "rock" || userChoise === "paper" || userChoise === "scissors"){
+
+    }else{
+        console.log("You Wrote Wrong")
+    }
 }else{
     console.log("You Are Cheated")
 }
