ea5db1f4f820524d20fc3c07991a74fbebb43c40
Nima Abed <abodnima84@gmail.com>
2023-05-05 10:21:00 +0330

    Show The Choises

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const computerChoise = choises[Math.round(Math.random() *3)]
 
 if (!(userChoise === "")){
     if(userChoise === "rock" || userChoise === "paper" || userChoise === "scissors"){
+        console.log(`My Choies Is: "${userChoise}"`)
+        console.log(`Computer Choies Is: "${computerChoise}"`)
         if(userChoise === computerChoise){
             console.log("It's Tie")
         }else{
