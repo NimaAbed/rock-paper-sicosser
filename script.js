49a92b46d1846b5fb4cf76bbb2a5bd17732356c2
Nima Abed <abodnima84@gmail.com>
2023-05-05 10:14:17 +0330

    Create If For Prompt Not Nll

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const userChoise = prompt("choise rock,paper,scissors")
 const choises = ["rock","paper","scissors"]
 const computerChoise = choises[Math.round(Math.random() *3)]
+
+if (!(userChoise === "")){
+    
+}else{
+    console.log("You Are Cheated")
+}
